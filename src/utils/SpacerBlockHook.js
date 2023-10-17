fd5c75248e57c741fc9242662d46a99cd69f3fd9
lboulanger <lboulanger@amphibee.fr>
2023-10-17 14:45:00 +0200

    🐛 fix(SpacerBlockHook.js): fix import statements and formatting issues ✨ feat(SpacerBlockHook.js): add support for responsive settings in Spacer block

--- src/utils/SpacerBlockHook.js
+++ src/utils/SpacerBlockHook.js
@@ -1,13 +1,12 @@
-import metadata from "../blocks/curvy/block.json";
+const {createHigherOrderComponent} = wp.compose;
+const {InspectorControls} = wp.blockEditor;
+const {BaseControl, PanelBody, ToggleControl} = wp.components;
+import {__} from "@wordpress/i18n";
 
-const {createHigherOrderComponent} = wp.compose
-const {InspectorControls} = wp.blockEditor
-const {BaseControl, PanelBody, __experimentalUnitControl, ToggleControl} = wp.components
-import { __ } from "@wordpress/i18n";
-const {Fragment} = wp.element
+const {Fragment} = wp.element;
 
 export default class SpacerBlockHook {
-	namespace = 'beeAddonsBlock/spacer';
+	namespace = "beeAddonsBlock/spacer";
 
 	static register() {
 		const instance = new this();
@@ -17,77 +16,102 @@ export default class SpacerBlockHook {
 	}
 
 	registerSpacerSettings() {
-		wp.hooks.addFilter('blocks.registerBlockType', `${this.namespace}/settings`, this.addSpacerSettings);
+		wp.hooks.addFilter(
+			"blocks.registerBlockType",
+			`${this.namespace}/settings`,
+			this.addSpacerSettings
+		);
 	}
 
 	addSpacerSettings(settings) {
-		if (settings.name !== 'core/spacer') return settings;
+		if (settings.name !== "core/spacer") return settings;
 		settings.supports = {...settings.supports, color: true};
-		settings.attributes = {...settings.attributes,
-			mobileHeight: {type: 'string', default: '0'},
-			showOnMobile: {type: 'boolean', default: false},
+		settings.attributes = {
+			...settings.attributes,
+			mobileHeight: {type: "string", default: "0"},
+			showOnMobile: {type: "boolean", default: false},
 		};
 		return settings;
 	}
 
 	registerSpacerControls() {
-		wp.hooks.addFilter('editor.BlockEdit', `${this.namespace}/controls`, this.addSpacerControls);
+		wp.hooks.addFilter(
+			"editor.BlockEdit",
+			`${this.namespace}/controls`,
+			this.addSpacerControls
+		);
 	}
 
 	addSpacerControls = createHigherOrderComponent((BlockEdit) => {
 		return (props) => {
-			if (props.name !== 'core/spacer') return <BlockEdit {...props} />;
+			if (props.name !== "core/spacer") return <BlockEdit {...props} />;
 			const {attributes, setAttributes} = props;
-			const {mobileHeight} = attributes;
 
-			return <Fragment>
-				<InspectorControls>
-					<PanelBody title={__('Responsive Settings', 'beeAddonsBlocks')} initialOpen={false}>
-						<div style={{display: "flex"}}>
-							<ToggleControl
-								onChange={(isChecked) => {
-									props.setAttributes({
-										showOnMobile: isChecked,
-									});
-								}}
-								checked={props.attributes.showOnMobile}
-							/>
-							<span>{__("Enable spacer on mobile", 'beeAddonsBlocks')}</span>
-						</div>
+			return (
+				<Fragment>
+					<InspectorControls>
+						<PanelBody
+							title={__("Responsive Settings", "beeAddonsBlocks")}
+							initialOpen={false}
+						>
+							<div style={{display: "flex"}}>
+								<ToggleControl
+									label={__("Enable spacer on mobile", "beeAddonsBlocks")}
+									checked={attributes.showOnMobile}
+									onChange={(isChecked) =>
+										setAttributes({showOnMobile: isChecked})
+									}
+								/>
+							</div>
 
-						{props.attributes.showOnMobile &&
-							<BaseControl label={__('Height', 'beeAddonsBlocks')}>
-								<__experimentalUnitControl type={'number'}
-														   min={0}
-														   step={1}
-														   value={mobileHeight || 80}
-														   onChange={(value) => setAttributes({mobileHeight: value})}>
-								</__experimentalUnitControl>
-							</BaseControl>
-						}
-					</PanelBody>
-				</InspectorControls>
-				<BlockEdit {...props} />
-			</Fragment>;
+							{attributes.showOnMobile && (
+								<BaseControl label={__("Height", "beeAddonsBlocks")}>
+									<input
+										type="number"
+										min="0"
+										step="1"
+										value={attributes.mobileHeight || "80"}
+										onChange={(event) =>
+											setAttributes({mobileHeight: event.target.value})
+										}
+									/>
+								</BaseControl>
+							)}
+						</PanelBody>
+					</InspectorControls>
+					<BlockEdit {...props} />
+				</Fragment>
+			);
 		};
-	}, 'addSpacerControls')
+	}, "addSpacerControls");
 
 	registerSpacerProps() {
-		wp.hooks.addFilter('blocks.getSaveContent.extraProps', `${this.namespace}/props`, this.addSpacerProps);
+		wp.hooks.addFilter(
+			"blocks.getSaveContent.extraProps",
+			`${this.namespace}/props`,
+			this.addSpacerProps
+		);
 	}
 
 	addSpacerProps(props, blockType, attributes) {
-		if (blockType.name !== 'core/spacer') return props;
+		if (blockType.name !== "core/spacer") return props;
 		const {mobileHeight, showOnMobile} = attributes;
-		const heightNumber = +mobileHeight.match(/\d+/)[0];
-		const showValue = +showOnMobile ? 'block' : 'hidden';
+		const heightNumber = parseInt(mobileHeight);
 
-		props.style = {...props.style, "--spacerMobileShow": showValue};
-		if (showOnMobile === true && mobileHeight) {
-			if(heightNumber > 0){
-				props.style = {...props.style, "--spacerMobileHeight": mobileHeight};
-			}
+		if (showOnMobile) {
+			props.style = {
+				...props.style,
+				"--spacerMobileShow": "block",
+			};
+		}
+
+		if (showOnMobile && heightNumber > 0) {
+			props.style = {
+				...props.style,
+				"--spacerMobileHeight": mobileHeight.includes('px') ? mobileHeight : `${mobileHeight}px`
+			};
 		}
 		return props;
 	}
+
 }
