7ca9ef2f4d16c93f700616c1a733257a036eaf99
lboulanger <lboulanger@amphibee.fr>
2024-03-07 22:00:15 +0100

    fix GenerateExcerptWithOpenAi

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-rich-text'), 'version' => '3c5e5a68d5c0f61c9324');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-rich-text'), 'version' => '1ab668baf7a7a29176d4');
--- src/utils/hooks/GenerateExcerptWithOpenAi.js
+++ src/utils/hooks/GenerateExcerptWithOpenAi.js
@@ -3,31 +3,31 @@ import { select } from '@wordpress/data';
 import {__} from "@wordpress/i18n";
 
 export default class GenerateExcerptWithOpenAi {
-	onGenerateAIExcerpt() {
+	async onGenerateAIExcerpt() {
 		const postContent = select('core/editor').getEditedPostAttribute('content');
 		try {
-			fetch(`${wpData.siteUrl}wp-json/bee-addons-blocks/v1/openai-generate-excerpt`, {
+			const response = await fetch(wpData.siteUrl + 'wp-json/bee-addons-blocks/v1/openai-generate-excerpt', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json'
 				},
 				body: JSON.stringify({ postContent })
-			})
-				.then(responseData => {
-					if (!responseData.ok) {
-						throw new Error('Failed to fetch');
-					}
-					console.log(responseData);
-					const excerptTextArea = document.querySelector('.editor-post-excerpt__textarea textarea');
-					if (excerptTextArea) {
-						excerptTextArea.textContent = responseData['excerpt'];
-						excerptTextArea.value = responseData['excerpt'];
-						excerptTextArea.dispatchEvent(new Event('input', { bubbles: true }));
-					} else {
-						console.error('No excerpt textarea found');
-					}
-				})
-				.catch(error => console.error(error));
+			});
+
+			if (response.ok) {
+				const responseData = await response.json();
+				const excerptTextArea = document.querySelector('.editor-post-excerpt__textarea textarea');
+				if (excerptTextArea) {
+					excerptTextArea.textContent = responseData['excerpt'];
+					excerptTextArea.value = responseData['excerpt'];
+					excerptTextArea.dispatchEvent(new Event('input', { bubbles: true }));
+				} else {
+					console.error('No excerpt textarea found');
+				}
+			} else {
+				const errorData = await response.text();
+				console.error('Request has failed : ' + response.status + ' - ' + errorData);
+			}
 		} catch (error) {
 			console.error(error);
 		}
